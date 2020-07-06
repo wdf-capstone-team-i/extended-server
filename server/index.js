39dd7109c8fa03446bff3d1205607b64a0305edb
Javier Montero <monterodotjavier@icloud.com>
2020-07-06 15:24:17 -0400

    added logging middleware morgan

--- server/index.js
+++ server/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-
+const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 8080;
 
-app.use(express.urlencoded({extended: false}));
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use((err, req, res, next) => {
+  res.send("Oops. Well, that's embarrassing");
+});
 
-
-app.use((err, req, res, next)=>{
-    res.send('Oops. Well, that\'s embarrassing');
-})
-
-app.listen(PORT, ()=> console.log('Serving on port: ', PORT))
+app.listen(PORT, () => console.log("Serving on port: ", PORT));
