fe29982f91df4fd8f527259de9bbe2899674296c
Javier Montero <monterodotjavier@icloud.com>
2020-07-06 15:28:19 -0400

    added compression  middleware

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const compression = require("compression");
 const app = express();
 const PORT = 8080;
 
@@ -7,6 +8,8 @@ app.use(morgan("dev"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(compression());
 
 app.use((err, req, res, next) => {
   res.send("Oops. Well, that's embarrassing");
