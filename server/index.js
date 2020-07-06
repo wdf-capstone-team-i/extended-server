849bd62c86d723dd3b0e8bdfe62d8ae879d09852
Gilbert <gilbertvelez96@github.com>
2020-07-06 14:45:45 -0400

    Set up the necessary server files. Set up User model

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
+
+app.use((err, req, res, next)=>{
+    res.send('Oops. Well, that\'s embarrassing');
+})
+
+app.listen(PORT, ()=> console.log('Serving on port: ', PORT))
