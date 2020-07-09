4d2d7237736fdd16fdd3f7cd1210b7c50d281309
SamMoskowicz <shmuelmoskowicz@gmail.com>
2020-07-09 12:41:33 -0400

    removed username from db

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://shmuel:Unix123455@localhost:5432/extended', {
+const db = new Sequelize('postgres://@localhost:5432/extended', {
     logging: false
 });
 
