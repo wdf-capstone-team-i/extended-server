bf92d7a9e42320457defb6cbb97a8821c98b1d26
Javier Montero <monterodotjavier@icloud.com>
2020-07-09 12:58:59 -0400

    feat: broswer site

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,5 @@
-const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://localhost:5432/extended');
+const Sequelize = require("sequelize");
+// Change to a variable
+const db = new Sequelize("postgres://localhost:5432/extended");
 
 module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const morgan = require("morgan");
 const compression = require("compression");
-const Socket = require('socket.io');
-const path = require('path');
+const Socket = require("socket.io");
+const path = require("path");
 const app = express();
 const PORT = 8080;
 const server = app.listen(PORT, () => console.log("Serving on port: ", PORT));
@@ -15,24 +15,18 @@ app.use(compression());
 
 // add routes here
 app.use("/api/users", require("./db/api/users"));
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
-app.use((err, req, res, next) => {
-  res.send("Oops. Well, that's embarrassing");
-});
-
-
-
-app.get('/chat', (req, res)=>{
-  res.sendFile(path.join(__dirname, '..','public/chat.html'))
-})
+app.use(express.static(path.join(__dirname, "..", "public")));
 
 const io = new Socket(server);
 
-io.on('connection', (socket) => {
-  console.log('Socket connection made!');
+io.on("connection", (socket) => {
+  console.log("Socket connection made!");
 
-  socket.on('msg:send', data => {
-      io.sockets.emit('msg:receive', data);
-  })
+  socket.on("msg:send", (data) => {
+    io.sockets.emit("msg:receive", data);
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.send("Oops. Well, that's embarrassing");
 });
