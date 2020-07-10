e43f5220fac205dc75824aa162a96f830167fe59
SamMoskowicz <shmuelmoskowicz@gmail.com>
2020-07-10 11:51:48 -0400

    added process.env.DATABASE_URL to db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
-const db = new Sequelize('postgres://@localhost:5432/extended', {
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/extended`, {
     logging: false
 });
 
