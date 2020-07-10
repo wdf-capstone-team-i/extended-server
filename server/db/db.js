dd01b4da7d7a126ec9feb2b94b113bd447b5d354
SamMoskowicz <shmuelmoskowicz@gmail.com>
2020-07-10 14:03:12 -0400

    added require secrets file

--- server/db/db.js
+++ server/db/db.js
@@ -1,4 +1,6 @@
 const Sequelize = require('sequelize');
+require('../../secrets')
+
 const db = new Sequelize(process.env.DATABASE_URL || `postgres://localhost:5432/extended`, {
     logging: false
 });
